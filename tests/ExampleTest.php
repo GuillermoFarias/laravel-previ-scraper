ecfe57f5def981f269f61e334158aa07ccc2fd83
Guillermo Farías <guillermodanilo08@gmail.com>
2021-04-24 12:13:37 -0400

    add github workflows

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gdanilo\PreviService\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testAlgoAlgo()
+    {
+        $this->assertTrue(true);
+    }
+}
